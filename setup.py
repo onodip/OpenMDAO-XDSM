1497d0e9b1b4d7e62278f42ff036cf772b604039
Peter ONODI <p.onodi@gmail.com>
2020-02-29 16:54:01 +0100

    setup updated

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup
 
 __version__ = "0.0"
+__author__ = 'Peter Onodi'
 
 setup(
     name='omxdsm',
     version=__version__,
     description="XDSM viewer plugin for OpenMDAO",
     keywords='openmdao_command openmdao xdsm optimization multidisciplinary multi-disciplinary',
-    author='Peter Onodi',
-    author_email='p.onodi@gmail.com',
+    author=__author__,
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Scientific/Engineering',
+        'Programming Language :: Python',
+    ],
     install_requires=[
         'openmdao>=2.10',
     ],
